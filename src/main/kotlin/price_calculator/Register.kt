7b11774255286cffe223fcf50163c3a5efcb1a10
EpicGuy4000 <enjakovic@gmail.com>
2024-03-21 17:54:24 +0100

    Kata - "Price Calculator - SELECTIVE" from Zoran Horvat

--- src/main/kotlin/price_calculator/Register.kt
+++ src/main/kotlin/price_calculator/Register.kt
@@ -1,11 +1,18 @@
 package org.kata.price_calculator
 
-class Register(private val product: Product, private val taxPercentage: Double, private val discountPercentage: Double = 0.0) {
+class Register(private val product: Product, private val taxPercentage: Double) {
+    var upc: Int = 0
+    var upcDiscountPercentage: Double = 0.0
+    var discountPercentage: Double = 0.0
+
     fun getReceipt(): Receipt {
         return Receipt().apply {
             price = product.price.getAmount()
             taxAmount = (product.price * Money(taxPercentage / 100.0)).getAmount()
-            discountAmount = (product.price * Money(discountPercentage / 100.00)).getAmount()
+            val universalDiscount = product.price * Money(discountPercentage / 100.00)
+            val selectiveDiscount = if (upc == product.upc) product.price * Money(upcDiscountPercentage / 100.00) else Money(0.0)
+
+            discountAmount = (universalDiscount + selectiveDiscount).getAmount()
             total = (product.price + taxAmount - discountAmount).getAmount()
         }
     }
--- src/test/kotlin/price_calculator/RegisterTest.kt
+++ src/test/kotlin/price_calculator/RegisterTest.kt
@@ -35,7 +35,9 @@ class RegisterTest {
     fun discount() {
         val product = Product("The Little Prince", 12345, Money(20.25))
 
-        val receipt = Register(product, 20.0, 15.0).getReceipt()
+        val receipt = Register(product, 20.0).apply {
+            discountPercentage = 15.0
+        }.getReceipt()
         assertEquals(4.05, receipt.taxAmount)
         assertEquals(3.04, receipt.discountAmount)
         assertEquals(20.25, receipt.price)
@@ -46,7 +48,9 @@ class RegisterTest {
     fun report_case1() {
         val product = Product("The Little Prince", 12345, Money(20.25))
 
-        val receipt = Register(product, 20.0, 15.0).getReceipt()
+        val receipt = Register(product, 20.0).apply {
+            discountPercentage = 15.0
+        }.getReceipt()
 
         val printed = receipt.print()
 
@@ -63,5 +67,37 @@ class RegisterTest {
         val printed = receipt.print()
 
         assertEquals("TOTAL: $24.30", printed)
+    }
+
+    @Test
+    fun selective_case1() {
+        val product = Product("The Little Prince", 12345, Money(20.25))
+
+        val receipt = Register(product, 20.0).apply {
+            discountPercentage = 15.0
+            upcDiscountPercentage = 7.0
+            upc = 12345
+        }.getReceipt()
+
+        assertEquals(20.25, receipt.price)
+        assertEquals(4.05, receipt.taxAmount)
+        assertEquals(4.46, receipt.discountAmount)
+        assertEquals(19.84, receipt.total)
+    }
+
+    @Test
+    fun selective_case2() {
+        val product = Product("The Little Prince", 12345, Money(20.25))
+
+        val receipt = Register(product, 21.0).apply {
+            discountPercentage = 15.0
+            upcDiscountPercentage = 7.0
+            upc = 789
+        }.getReceipt()
+
+        assertEquals(20.25, receipt.price)
+        assertEquals(4.25, receipt.taxAmount)
+        assertEquals(3.04, receipt.discountAmount)
+        assertEquals(21.46, receipt.total)
     }
 }
