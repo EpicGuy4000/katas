dba7d71e024bdebddb6783135339dbe04eccd3cc
EpicGuy4000 <enjakovic@gmail.com>
2024-06-13 16:51:21 +0200

    Kata - "Row With Maximum Ones" from DesignGurus

---
+++ src/main/kotlin/RowWithMaximumOnes.kt
@@ -0,0 +1,8 @@
+package org.kata
+
+class RowWithMaximumOnes {
+    companion object {
+        fun getMostOnes(matrix: Array<Array<Int>>): Pair<Int, Int> =
+            matrix.withIndex().maxOfWith({ p0, p1 -> p0!!.second - p1!!.second }) { (index, row) -> Pair(index, row.sum()) }
+    }
+}
