ce977351f91048fec0dde287f0cbb5d21f701f69
EpicGuy4000 <enjakovic@gmail.com>
2024-07-08 17:01:33 +0200

    Kata - "Decimal to Binary Conversion" from DesignGurus

---
+++ src/test/kotlin/DecimalToBinaryConversionTest.kt
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import org.kata.DecimalToBinaryConversion
+
+class DecimalToBinaryConversionTest {
+    @TestFactory
+    fun convertToBinary() = listOf(
+        2 to "10",
+        7 to "111",
+        18 to "10010",
+    ).map { (input, expected) ->
+        DynamicTest.dynamicTest("Checks if $input is converted correctly") {
+            Assertions.assertEquals(expected, DecimalToBinaryConversion.convert(input))
+        }
+    }
+}
