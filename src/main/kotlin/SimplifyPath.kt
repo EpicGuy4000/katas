dde3d68c3249f9ad83880e98b02f96199bdf83cb
EpicGuy4000 <enjakovic@gmail.com>
2024-07-09 16:23:30 +0200

    Kata - "Simplify Path" from DesignGurus

---
+++ src/main/kotlin/SimplifyPath.kt
@@ -0,0 +1,30 @@
+package org.kata
+
+import java.util.*
+
+class SimplifyPath {
+    companion object {
+        fun simplify(path: String): String {
+            val stack = Stack<String>()
+
+            path.split("/").forEach {
+                when (it) {
+                    ".." -> if (!stack.isEmpty()) stack.pop()
+                    "" -> {}
+                    "." -> {}
+                    else -> stack.push(it)
+                }
+            }
+
+            val output = buildString {
+                if (stack.isEmpty()) append("/")
+
+                while (!stack.empty()) {
+                    insert(0, "/" + stack.pop())
+                }
+            }
+
+            return output
+        }
+    }
+}
