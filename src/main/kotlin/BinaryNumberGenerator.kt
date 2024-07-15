fc39fa734e46ea9823b5ca8c6e6c85ad5412927c
EpicGuy4000 <enjakovic@gmail.com>
2024-07-15 13:17:29 +0200

    Kata - "Generate Binary Numbers from 1 to N" from DesignGurus

---
+++ src/main/kotlin/BinaryNumberGenerator.kt
@@ -0,0 +1,21 @@
+package org.kata
+
+import java.util.*
+
+object BinaryNumberGenerator {
+    fun generate(n: Int) : List<String> {
+        val out = mutableListOf<String>()
+        val queue: Queue<String> = LinkedList()
+
+        queue.add("1")
+
+        repeat (n) {
+            val c = queue.poll()
+            out.add(c)
+            queue.add(c + "0")
+            queue.add(c + "1")
+        }
+
+        return out
+    }
+}
