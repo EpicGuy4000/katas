1afc9f494ed52b0c0055dc6d8abf03c5e67c9dbb
EpicGuy4000 <enjakovic@gmail.com>
2024-07-16 15:47:02 +0200

    Kata - "Find if Doubly Linked List is a Palindrome" from DesignGurus

---
+++ src/main/kotlin/DoublyLinkedListNode.kt
@@ -0,0 +1,32 @@
+package org.kata
+
+data class DoublyLinkedListNode(
+    val value: Int,
+    var next: DoublyLinkedListNode? = null,
+    var prev: DoublyLinkedListNode? = null
+) {
+    override fun toString(): String = value.toString()
+
+    companion object {
+        fun fromList(list: List<Int>): DoublyLinkedListNode? {
+            if (list.isEmpty()) return null
+
+            val nodes = list.map { DoublyLinkedListNode(it) }
+            for (i in nodes.indices) {
+                if (i > 0) nodes[i].prev = nodes[i - 1]
+                if (i < nodes.lastIndex) nodes[i].next = nodes[i + 1]
+            }
+            return nodes[0]
+        }
+    }
+
+    fun printAfter(): List<Int> {
+        return buildList {
+            var curr: DoublyLinkedListNode? = this@DoublyLinkedListNode
+            while (curr != null) {
+                add(curr.value)
+                curr = curr.next
+            }
+        }
+    }
+}
